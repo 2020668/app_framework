1ad20bfb4ecd829d333f1f8d228f04fc6008d1c4
“2020668” <keen2020@outlook.com>
2019-08-30 07:40:06 +0800

    version 20190830

--- util/01.py
+++ util/01.py
@@ -22,15 +22,15 @@ desired_caps = {}
 
 desired_caps["platformName"] = "Android"  # 平台名称
 desired_caps["platformVersion"] = "9"  # 平台版本
-desired_caps["deviceName"] = "Android Emulator"  # 设备名称
+desired_caps["deviceName"] = "mi8_lite"  # 设备名称
 desired_caps["appPackage"] = 'com.cashier.jiutongshanghu'  # 应用包名
-desired_caps["appActivity"] = 'com.cashier.jiutongshanghu.activity.WelcomeActivity'  # 应用入口页面
+desired_caps["appActivity"] = 'com.cashier.jiutongshanghu/.home.home_main.activity.StartActivity'  # 应用入口页面
 desired_caps["noReset"] = True  # 不重置应用的状态
 
 # desired_caps["chromedriverExecutable"] = 'D:\\ChromeDrivers\\chrome39-42\\chromedriver.exe'
 
 # 2、连接appium desktop，向appium发送请求。在哪个设备打开哪个app
-driver = webdriver.Remote('http://192.168.124.6:4723/wd/hub', desired_caps)
+driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
 
 # loc = (MobileBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("乐付")')
 # WebDriverWait(driver, 20).until(ec.visibility_of_all_elements_located(loc))
@@ -44,16 +44,16 @@ driver = webdriver.Remote('http://192.168.124.6:4723/wd/hub', desired_caps)
 # loc = (MobileBy.CLASS_NAME, 'android.webkit.WebView')
 # WebDriverWait(driver, 20).until(ec.visibility_of_all_elements_located(loc))
 # time.sleep(2)
-#
+
 # # 获取当前所有的contexts
 # cons = driver.contexts
 # print(cons)
-#
+
 # # 切换到 webview --- 切换到html页面
 # driver.switch_to.context(cons[1])
 # time.sleep(2)
 # print("当前的context: ", driver.current_context)
-#
+
 # print("=======================进入web自动化======================")
 # 怎么得到html页面，并进行元素定位？？
 # 1、 工具 - uc devtools
