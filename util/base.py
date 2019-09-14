9566f7b169878e185399047cd8aa8a9b42101a46
“2020668” <keen2020@outlook.com>
2019-09-14 17:47:55 +0800

    version 20190914

--- util/base.py
+++ util/base.py
@@ -12,8 +12,11 @@ E-mail:keen2020@outlook.com
 
 """
 
-
 from appium import webdriver
+from appium.webdriver.common.touch_action import TouchAction
+from appium.webdriver.common.mobileby import MobileBy
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 import time
 
 # desired_caps["automationName"] = "UiAutomator2"
@@ -21,7 +24,7 @@ import time
 desired_caps = {}
 
 desired_caps["platformName"] = "Android"  # 平台名称
-desired_caps["platformVersion"] = "5.1"  # 平台版本,雷电模拟器的安卓版本是5.1
+desired_caps["platformVersion"] = "9"  # 平台版本,雷电模拟器的安卓版本是5.1,真机以实际版本为准
 desired_caps["deviceName"] = "mi8_lite"  # 设备名称，可以随便写
 desired_caps["appPackage"] = 'com.cashier.jiutongshanghu'  # 应用包名 com.cashier.jiutongshanghu
 desired_caps["appActivity"] = 'com.cashier.jiutongshanghu.home.home_main.activity.StartActivity'  # 应用入口页面
@@ -32,13 +35,69 @@ desired_caps["noReset"] = True  # 不重置应用的状态
 # 2、连接appium desktop，向appium发送请求。在哪个设备打开哪个app
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
 
-# loc = (MobileBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("乐付")') 一次使用多个方法组合定位
-# WebDriverWait(driver, 20).until(ec.visibility_of_all_elements_located(loc))
-# # 获取当前所有的contexts
-# cons = driver.contexts
-# print(cons)
+driver.find_element_by_android_uiautomator(
+    'new UiSelector().resourceId("com.cashier.jiutongshanghu:id/ed_login_phone")').send_keys("13072721092")
 
+driver.find_element_by_android_uiautomator(
+    'new UiSelector().resourceId("com.cashier.jiutongshanghu:id/ed_login_password")').send_keys("123456")
+
+# driver.find_element_by_android_uiautomator(
+#     'new UiSelector().resourceId("com.cashier.jiutongshanghu:id/iv_login_jizhu")').click()
+
+driver.find_element_by_android_uiautomator(
+    'new UiSelector().resourceId("com.cashier.jiutongshanghu:id/but_login")').click()
+
+# loc = MobileBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("账单")'
+# WebDriverWait(driver, 20).until(ec.visibility_of_element_located(loc))
 # driver.find_element(*loc).click()
+#
+# # 所有页面
+# loc = (MobileBy.ID, 'com.cashier.jiutongshanghu:id/tv_status')
+# WebDriverWait(driver, 20).until(ec.visibility_of_all_elements_located(loc))
+# time.sleep(1)
+#
+# size = driver.get_window_size()
+# # 滑动之前的页面
+# old = ""
+# # 当前的页面
+# new = driver.page_source
+#
+# while old != new:
+#     try:
+#         driver.find_element(MobileBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("15:40:48")')
+#         print("找到 指定订单 元素了")
+#     except:
+#         old = new  # 滑动之前,将当前的页面赋值给old
+#         driver.swipe(size["width"] * 0.9, size["height"] * 0.5, size["width"] * 0.1, size["height"] * 0.5, 200)
+#         loc = MobileBy.ID, 'com.cashier.jiutongshanghu:id/tv_status'
+#         WebDriverWait(driver, 20).until(ec.visibility_of_all_elements_located(loc))
+#         time.sleep(1)
+#         # 重新获取当前页面
+#         new = driver.page_source
+#     else:
+#         print("找到 指定订单 元素了")
+#         break
+
+# 点击 我的
+# loc = MobileBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("账单")'
+loc = (MobileBy.ID, 'new UiSelector().resourceId("com.cashier.jiutongshanghu:id/tv_tab_title")')
+WebDriverWait(driver, 20).until(ec.visibility_of_element_located(loc))
+print("找到了")
+driver.find_element(*loc)[2].click()
+
+# 点击 安全退出
+loc = MobileBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("安全退出")'
+WebDriverWait(driver, 20).until(ec.visibility_of_element_located(loc))
+driver.find_element(*loc).click()
+
+# 点击确定
+loc = MobileBy.ID, "com.cashier.jiutongshanghu:id/dialog_normal_rightbtn"
+
+# 点击取消
+# loc = MobileBy.ID, "com.cashier.jiutongshanghu:id/dialog_normal_leftbtn"
+
+WebDriverWait(driver, 20).until(ec.visibility_of_all_elements_located(loc))
+driver.find_element(*loc).click()
 
 # # 等待webview元素出现
 # loc = (MobileBy.CLASS_NAME, 'android.webkit.WebView')
